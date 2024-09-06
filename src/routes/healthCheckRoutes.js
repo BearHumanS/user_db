eaea13e8748001d521737b706be68841d72d1697
BearHumanS <bye0651@naver.com>
2024-09-07 02:31:36 +0900

    test: CI/CD TEST

--- src/routes/healthCheckRoutes.js
+++ src/routes/healthCheckRoutes.js
@@ -4,7 +4,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).send("OK");
+  res.status(200).send("OKKKK");
 });
 
 export default router;
