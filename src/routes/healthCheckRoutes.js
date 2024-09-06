45444ce7b4c5a68a2e560b32d6a63f03f798e584
BearHumanS <bye0651@naver.com>
2024-09-07 02:33:19 +0900

    chore: test 전으로

--- src/routes/healthCheckRoutes.js
+++ src/routes/healthCheckRoutes.js
@@ -4,7 +4,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).send("OKKKK");
+  res.status(200).send("OK");
 });
 
 export default router;
