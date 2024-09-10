48dadf60c59e350faa85241c50957f0e4b274504
BearHumanS <bye0651@naver.com>
2024-09-10 17:04:11 +0900

    refactor: 사용자 인증 route 추가 및 기타 보안 설정

--- src/app.js
+++ src/app.js
@@ -3,16 +3,30 @@ import userRoutes from "./routes/userRoutes.js";
 import healthCheckRoutes from "./routes/healthCheckRoutes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import { authenticate } from "./middleware/authMiddleware.js";
+
+// 환경 변수 로드
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// 미들웨어 설정
 app.use(express.json()); // JSON 요청 본문 파싱
 app.use(cookieParser()); // 쿠키 파서 미들웨어 추가
+app.use(helmet()); // 보안 관련 HTTP 헤더 설정
+app.use(morgan("combined")); // 요청/응답 로깅
 
+// CORS 설정
 app.use(
   cors({
-    origin: process.env.URL, // 허용할 도메인 (필요에 따라 변경)
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.URL
+        : "http://localhost:3000",
     credentials: true, // 쿠키와 함께 요청을 보낼 수 있도록 허용
   })
 );
@@ -21,8 +35,21 @@ app.use(
 app.use("/users", userRoutes);
 // AWS Elastic BeansTalk 로드 벨런서 상태 체크
 app.use("/", healthCheckRoutes);
+// 사용자 관련 Route관리
+app.use("/protected", authenticate);
+
+// 404 핸들링
+app.use((_, res, _) => {
+  res.status(404).json({ message: "Not Found" });
+});
+
+// 에러 핸들링 미들웨어
+app.use((err, _, res, _) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error" });
+});
 
 // 서버 실행
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}PORT`);
+  console.log(`Server is running on port ${PORT}`);
 });
